087556203c632d963c0e2f69cad406ae126fc754
jana sanjakdar <jana.sanjakdar@hotmail.com>
2021-04-28 07:33:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code here
+function sturdayFun (activity){
+  if(activity==''){
+    concole.log ('This saturday,I want to roller-skate!')
+  }
+  console.log ('This saturday, I want to ${activity}!')
+}
