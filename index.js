1559db9773750e6ae63583d07e790a1a965b2d03
jana sanjakdar <jana.sanjakdar@hotmail.com>
2021-04-28 07:45:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 function sturdayFun (activity='roller-skate'){
   return `This saturday,I want to ${activity}!`
 }
+
+const mondayWork = function(activity= 'go to the office'){
+  return `This Monday I will ${activity}.`
+}
