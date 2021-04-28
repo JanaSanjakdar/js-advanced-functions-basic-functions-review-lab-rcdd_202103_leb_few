afec8eef65bfcefd4b5cfc4c1ba2555fc9fdb78b
jana sanjakdar <jana.sanjakdar@hotmail.com>
2021-04-28 08:10:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,9 +16,14 @@ function wrapAdjective (wrap='*'){
 const Calculator = {
   add: function(a,b){
     return a+b;
-  }
+  },
   subtract: function(a,b){
     return a-b;
-  }
-
+  },
+  multiply: function(a,b) {
+          return a*b;
+      },
+      divide: function(a,b) {
+          return a/b;
+      }
 }
